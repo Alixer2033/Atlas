f0a89ee693b93dd2cf760f954416a7407db0f1e7
Alex Wilson <Alixer2033@gmail.com>
2020-01-23 12:10:14 +0300

    Update List.hpp
    a

--- List.hpp
+++ List.hpp
@@ -1,1 +1,31 @@
-测试
+#ifndef List_hpp
+#define List_hpp
+#include <string>
+using std::string;
+
+struct Node {
+	int Data_;
+	Node* next_;
+	Node() :next_(nullptr) {}
+	Node(const int& Data) :Data_(Data), next_(nullptr) {}
+};
+
+class List {
+private:
+	string Name_;
+	Node* head_;
+	Node* tail_;
+public:
+	List():head_(nullptr),tail_(nullptr){}
+	~List() {
+		while (head_) {
+			tail_ = head_->next_;
+			delete head_;
+			head_ = tail_;
+		}
+	}
+
+
+};
+
+#endif
