a086423b962a4ac585141d40fd225f4b72b620de
Atlas <Alixer2033@gmail.com>
2020-02-15 17:01:39 +0300

    Create Matrix.hpp

---
+++ Matrix.hpp
@@ -0,0 +1,31 @@
+#ifndef Matrix_hpp
+#define Matrix_hpp
+
+template <class T>
+class Matrix {
+private:
+	T* Matrix_;
+	int Col_;
+	int Row_;
+	int Size_;
+public:
+	Matrix(const int& Col, const int& Row) :
+		Col_(Col),
+		Row_(Row),
+		Size_(Col_* Row_),
+		Matrix_(new T[Size_])
+	{}
+	Matrix(Matrix<T>&& obj)noexcept :
+		Col_(obj.Col_),
+		Row_(obj.Row_),
+		Size_(obj.Size_),
+		Matrix_(obj.Matrix_)
+	{
+		obj.Matrix_ = nullptr;
+	}
+	~Matrix() {
+		delete[] Matrix_;
+	}
+};
+
+#endif
